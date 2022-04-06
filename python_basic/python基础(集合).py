496c51079ea6e7b4c1fdf7be74723899fd769293
003398 <liuxiong@fcbox.com>
2022-04-06 18:49:01 +0800

    添加

---
+++ python_basic/python基础(集合).py
@@ -0,0 +1,18 @@
+# -*- encoding=utf8 -*- 
+'''
+@Time:  2022/4/6-15:01
+@User:  Administrator
+@File:  python基础(集合).py
+@Email：  liuxiong@fcbox.com
+'''
+'''
+集合（Set）
+集合是无序和无索引的集合。在 Python 中，集合用花括号编写
+
+'''
+# 集合创建
+s2 = {"apple", "banana", "cherry"}
+s1 = set("123")
+print(s1)
+print(s2, type(s2))
+
